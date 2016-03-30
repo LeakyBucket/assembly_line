b80a8a757c276dccf836f3e1e7043b4c56466ea5
Glen Holcomb <DamnBigMan@gmail.com>
2016-03-29 19:24:06 -0600

    Finishes initial `Server` module

--- lib/dag/job_queue/server.ex
+++ lib/dag/job_queue/server.ex
@@ -3,23 +3,48 @@ defmodule Dag.JobQueue.Server do
   Manages job queues
   """
 
-  def start_link(name) do
-    Agent.start_link(fn -> [] end, name: name)
+  defstruct work: [], finished: MapSet.new([])
+
+  def start_link(name, work) do
+    Agent.start_link(fn -> %__MODULE__{work: work} end, name: name)
   end
 
   def next_for(name) do
-    Agent.get(name, fn [next | rest] ->
+    Agent.get(name, fn %__MODULE__{work: [next | _rest]} ->
       next
     end)
   end
 
-  def complete_current(name) do
-    Agent.update(name, fn [current | remaining] ->
-      remaining
+  def get_completed(name) do
+    Agent.get(name, fn %__MODULE__{finished: completed} ->
+      completed
     end)
+  end
+
+  def complete_current_set(name) do
+    Agent.update(name, fn %__MODULE__{work: [current | remaining], finished: finished} ->
+      %__MODULE__{work: remaining, finished: MapSet.union(finished, __MODULE__.to_set(current))}
+    end)
+  end
+
+  def complete_task(name, task) do
+    Agent.update(name, __MODULE__, :add_to_finished, [task])
   end
 
   def finished(name) do
     Agent.stop name
+  end
+
+  def add_to_finished(%__MODULE__{work: work, finished: finished}, tasks) do
+    %__MODULE__{work: work, finished: MapSet.union(finished, to_set(tasks))}
+  end
+
+  def to_set(jobs) when is_list(jobs) do
+    Enum.reduce(jobs, MapSet.new([]), fn job, set ->
+      MapSet.put set, job
+    end)
+  end
+  def to_set(jobs) do
+    MapSet.new [jobs]
   end
 end
---
+++ test/dag/job_queue/server_test.exs
@@ -0,0 +1,34 @@
+defmodule Dag.JobQueue.ServerTest do
+  use ExUnit.Case
+
+  alias Dag.JobQueue.Server
+
+  @name :test
+
+  setup do
+    {:ok, agent} = Server.start_link(@name, [[:a, :b], :c])
+    {:ok, agent: agent}
+  end
+
+  test "fetching the next task" do
+    assert [:a, :b] = Server.next_for @name
+  end
+
+  test "completing the current task set" do
+    Server.complete_current_set :test
+    expected = MapSet.new([:a, :b])
+
+    assert ^expected = Server.get_completed :test
+  end
+
+  test "getting completed tasks" do
+    assert %MapSet{} = Server.get_completed :test
+  end
+
+  test "completing a single task" do
+    Server.complete_task :test, :a
+    expected = MapSet.new([:a])
+
+    assert ^expected = Server.get_completed :test
+  end
+end
